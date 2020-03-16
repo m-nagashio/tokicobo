ee23d1424710aad7ff60a71077a5e31d66d5a7ed
m-nagashio <m.nagashio.n618m@gmail.com>
2020-03-16 11:14:11 +0900

    fix bug.

--- lambda/analyze-sentiment/index.js
+++ lambda/analyze-sentiment/index.js
@@ -2,15 +2,6 @@ const request = require('sync-request');
 const mysql = require('mysql');
 const util = require('util');
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host : 'tokicobo-mysql-public.cjzqiuaqkjbq.ap-northeast-1.rds.amazonaws.com',
-  user : 'admin',
-  password : '5RULDddNdeF57zKhSkWV',
-  port : 3306,
-  database: 'tokicobo'
-})
-
 const ACCESS_TOKEN_URL = 'https://api.ce-cotoha.com/v1/oauth/accesstokens';
 const SENTIMENT_API_URL = 'https://api.ce-cotoha.com/api/dev/nlp/v1/sentiment';
 
@@ -20,6 +11,14 @@ const SENTIMENT_TYPE_NEGATIVE = 0;
 const SENTIMENT_TYPE_POSITIVE = 1;
 
 async function fetchTalkKeywords(sentimentType, talkCount) {
+  const pool = mysql.createPool({
+    connectionLimit: 10,
+    host : 'tokicobo-mysql-public.cjzqiuaqkjbq.ap-northeast-1.rds.amazonaws.com',
+    user : 'admin',
+    password : '5RULDddNdeF57zKhSkWV',
+    port : 3306,
+    database: 'tokicobo'
+  })
   pool.query = util.promisify(pool.query)
   try {
     var results = await pool.query('select * from talk_keyword where sentiment_type = ? and talk_count = ?', 
@@ -33,7 +32,7 @@ async function fetchTalkKeywords(sentimentType, talkCount) {
 
 exports.handler = async (event) => {
   const inputText = event.input_text;
-  // const inputText = "行きたい";
+
   var accessTokenRes = request('POST', ACCESS_TOKEN_URL, {
     headers: {
       "content-type": "application/json"
@@ -59,36 +58,25 @@ exports.handler = async (event) => {
   const sentimentBody = JSON.parse(sentimentRes.getBody('utf8'));
   const sentiment = sentimentBody.result.sentiment;
 
-  // if (sentiment == SENTIMENT_NEGATIVE) {
-  //   return { statusCode: 200, body: SENTIMENT_TYPE_NEGATIVE};
-  // }
+  if (sentiment == SENTIMENT_NEGATIVE) {
+    return { statusCode: 200, body: SENTIMENT_TYPE_NEGATIVE};
+  }
 
-  // const talkCount = event.talk_count;
-  // const talkCount = 1;
+  const talkCount = event.talk_count;
 
-  // var sentimentType = SENTIMENT_TYPE_NEGATIVE
-  // var response = {
-  //   statusCode: 200,
-  //   body: sentimentType,
-  // };
+  var sentimentType = SENTIMENT_TYPE_NEGATIVE;
 
-  // fetchTalkKeywords(SENTIMENT_TYPE_POSITIVE, talkCount).then(
-  //   result => {
-  //     result.some(function(record){
-  //       if (inputText.indexOf(record.keyword) != -1) {
-  //         sentimentType = SENTIMENT_TYPE_POSITIVE;
-  //         return true;
-  //       }
-  //     });
-  //     console.log("sentimentType");
-  //     console.log(sentimentType);
-  //   }
-  // );
+  const keywards = await fetchTalkKeywords(SENTIMENT_TYPE_POSITIVE, talkCount);
+  keywards.some(function(record){
+    if (inputText.indexOf(record.keyword) != -1) {
+      sentimentType = SENTIMENT_TYPE_POSITIVE;
+      return true;
+    }
+  });
 
-  // console.log(sentimentType);
   const response = {
     statusCode: 200,
-    body: sentiment,
+    body: sentimentType,
   };
   return response;
 };
